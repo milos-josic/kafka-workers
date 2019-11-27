95229661ab413e23ae805d5685ebf038d23894da
milos.josic@sap.com <milos.josic@sap.com>
2019-11-27 20:28:30 +0100

    small cleanup

--- src/scheduler/scheduler-service.ts
+++ src/scheduler/scheduler-service.ts
@@ -14,27 +14,10 @@ export class SchedulerService {
         private kafkaFactory: IKafkaClientFactory,
         private taskCreator: ITaskCreator) {
         this.producer = this.kafkaFactory.getProducer();
-
-        // this.producer.on('error', (error) => {
-        //     console.error(error);
-        //     //return reject(error);
-        // })
     }
 
     public async scheduleTasks(): Promise<any> {
-        return new Promise(async (resolve, reject) => {
-            console.log('schedule tasks');
-            //how can we make schedulers concurent??
-            //we should when new document get's created create a task on kafka, but what if it fails??
-
-            //check if there are new files since last check 
-
-            debugger;
-            await this.taskCreator.create('Example', 'tenantID', { Id: this.taskId++, createdOn: new Date() });
-
-
-            return resolve();
-        })
+        await this.taskCreator.create('Example', 'tenantID', { Id: this.taskId++, createdOn: new Date() });
     }
 
     public async scheduleCleanUpTask(): Promise<boolean> {
